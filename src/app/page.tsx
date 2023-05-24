85dc1281da370c03462e14a99660487fcf59c53a
Rayan Kazi <rayankazi7515@gmail.com>
2023-05-23 21:26:33 -0700

    got started done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Table from "@/components/Table";
+
+const session = null;
+
 export default function Home() {
-  return <main className=""></main>;
+  if (!session) throw new Error("Auth is required to access this page");
+
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <div className="overflow-x-auto">
+        <Table />
+      </div>
+    </main>
+  );
 }
